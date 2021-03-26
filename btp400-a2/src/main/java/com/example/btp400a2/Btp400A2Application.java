5d0eebbd40d4e37cab825dc567d3fd03c22b9866
Harsh K <hkmkeswani@myseneca.ca>
2021-03-25 23:22:54 -0400

    created project

---
+++ btp400-a2/src/main/java/com/example/btp400a2/Btp400A2Application.java
@@ -0,0 +1,13 @@
+package com.example.btp400a2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Btp400A2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Btp400A2Application.class, args);
+	}
+
+}
---
+++ btp400-a2/src/test/java/com/example/btp400a2/Btp400A2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.btp400a2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Btp400A2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
