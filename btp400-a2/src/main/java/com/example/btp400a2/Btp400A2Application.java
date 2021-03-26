1d0fe27db848269e5642458c09743674562d1046
Harsh K <hkmkeswani@myseneca.ca>
2021-03-26 00:07:00 -0400

    helo wrold

--- btp400-a2/src/main/java/com/example/btp400a2/Btp400A2Application.java
+++ btp400-a2/src/main/java/com/example/btp400a2/Btp400A2Application.java
@@ -2,7 +2,6 @@ package com.example.btp400a2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Btp400A2Application {
@@ -10,11 +9,5 @@ public class Btp400A2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Btp400A2Application.class, args);
 	}
-	
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
+
 }
-
-
