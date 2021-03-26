3009009d4321b179d7c99325f825f2dafc830980
Harsh K <hkmkeswani@myseneca.ca>
2021-03-26 00:02:51 -0400

    simple helo world

--- btp400-a2/src/main/java/com/example/btp400a2/Btp400A2Application.java
+++ btp400-a2/src/main/java/com/example/btp400a2/Btp400A2Application.java
@@ -1,12 +1,8 @@
 package com.example.btp400a2;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Btp400A2Application {
@@ -15,19 +11,9 @@ public class Btp400A2Application {
 		SpringApplication.run(Btp400A2Application.class, args);
 	}
 	
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
 	}
 }
 
