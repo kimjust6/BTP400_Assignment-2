7a5b159a33d54178cddca43ef3b8582465a991aa
Harsh K <hkmkeswani@myseneca.ca>
2021-03-25 23:59:36 -0400

    hello world

--- btp400-a2/src/main/java/com/example/btp400a2/Btp400A2Application.java
+++ btp400-a2/src/main/java/com/example/btp400a2/Btp400A2Application.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Btp400A2Application {
@@ -12,8 +11,11 @@ public class Btp400A2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Btp400A2Application.class, args);
 	}
+	
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-	return String.format("Hello %s!", name);
+		return String.format("Hello %s!", name);
 	}
 }
+
+
