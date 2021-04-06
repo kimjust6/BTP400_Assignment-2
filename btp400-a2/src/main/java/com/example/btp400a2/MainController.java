7653fcdf1e3bfad908e881f311b74e21080b0371
Harsh K <hkmkeswani@myseneca.ca>
2021-04-06 00:11:44 -0400

    /wallet/:id routing using thymeleaf

--- btp400-a2/src/main/java/com/example/btp400a2/MainController.java
+++ btp400-a2/src/main/java/com/example/btp400a2/MainController.java
@@ -11,37 +11,33 @@ import bankingV20_0.Bank;
 
 @Controller
 public class MainController {
-	
+
 	@GetMapping("/")
 	public String homePage(Model model) {
 		model.addAttribute("group", "Group 4");
 		return "home";
 	}
-	
+
 	@GetMapping(path = "/wallet/{walletID}")
-	public String walletInfo(@PathVariable String walletID) throws Exception {
+	public String walletInfo(Model model, @PathVariable String walletID) throws Exception {
 		Bank aBank = new Bank();
 		Account anAccount = aBank.getAccount(Integer.parseInt(walletID));
 
 		String imageBase64 = QRCodeGenerator.generateQRCodeImage(anAccount.getPubAddress(), 250, 250);
-		// imageBase64 =
-		// QRCodeGenerator.generateQRCodeImage("991A38BED0D022D6622E9AD47513E2A14AC0DA58F15D8AFC81075DEC11CAF29D",250,250);
 		
-		
-		
-		return String.format("<div>Public Address: <a href=%s target=_blank>%s</a></div>",
-				"https://creeper.banano.cc/explorer/account/" + anAccount.getPubAddress(), anAccount.getPubAddress())
-				+ String.format("<div>Account Number: %s</div>", anAccount.getAccountNo())
-				+ String.format("<div>Balance: %s</div>", anAccount.getBalance())
-				+ String.format("<img src=\"%s\"></img>", "data:image/png;base64," + imageBase64);
-		
-	}
+		// can be made into an attribute collection
+		model.addAttribute("publicAddr", anAccount.getPubAddress());
+		model.addAttribute("accNo", anAccount.getAccountNo());
+		model.addAttribute("balAmt", anAccount.getBalance());
+		model.addAttribute("qrImg", "data:image/png;base64," + imageBase64);
 
+		return "wallet";
+	}
 	
 	
-	
+
 	// this one isnt set up -------------------------------
-	
+
 	@GetMapping("/wallet")
 	public String qrcode() throws Exception {
 		return getQRCode();
