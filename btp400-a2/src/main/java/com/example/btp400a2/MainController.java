2c10b88139626dd5b9bb1c114d0021e5e6e0a27b
Harsh K <hkmkeswani@myseneca.ca>
2021-04-05 23:54:57 -0400

    testing thymeleaf and html page routing

--- btp400-a2/src/main/java/com/example/btp400a2/MainController.java
+++ btp400-a2/src/main/java/com/example/btp400a2/MainController.java
@@ -1,29 +1,23 @@
 package com.example.btp400a2;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import QRCode.QRCodeGenerator;
 import bankingV20_0.Account;
 import bankingV20_0.Bank;
-import uk.oczadly.karl.jnano.rpc.util.wallet.WalletActionException;
 
-@RestController
-
-public class HelloController {
-
-	@RequestMapping("/")
-	public String index() {
-		return query();
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String homePage(Model model) {
+		model.addAttribute("group", "Group 4");
+		return "home";
 	}
-
+	
 	@GetMapping(path = "/wallet/{walletID}")
 	public String walletInfo(@PathVariable String walletID) throws Exception {
 		Bank aBank = new Bank();
@@ -32,40 +26,25 @@ public class HelloController {
 		String imageBase64 = QRCodeGenerator.generateQRCodeImage(anAccount.getPubAddress(), 250, 250);
 		// imageBase64 =
 		// QRCodeGenerator.generateQRCodeImage("991A38BED0D022D6622E9AD47513E2A14AC0DA58F15D8AFC81075DEC11CAF29D",250,250);
+		
+		
+		
 		return String.format("<div>Public Address: <a href=%s target=_blank>%s</a></div>",
 				"https://creeper.banano.cc/explorer/account/" + anAccount.getPubAddress(), anAccount.getPubAddress())
 				+ String.format("<div>Account Number: %s</div>", anAccount.getAccountNo())
 				+ String.format("<div>Balance: %s</div>", anAccount.getBalance())
 				+ String.format("<img src=\"%s\"></img>", "data:image/png;base64," + imageBase64);
-
+		
 	}
 
+	
+	
+	
+	// this one isnt set up -------------------------------
+	
 	@GetMapping("/wallet")
 	public String qrcode() throws Exception {
 		return getQRCode();
-	}
-
-	public String query() {
-		String str = "error";
-
-		try (Connection conn = DriverManager.getConnection(
-				"jdbc:postgresql://ec2-54-209-43-223.compute-1.amazonaws.com:5432/d19rc88931g1bi", "luwrnpzmqrzvln",
-				"9b76f4cfa5a87feb4cf28e8b90e485b183bca39b2b20c09e323b4a04b524b2ce");
-				Statement statement = conn.createStatement()) {
-
-			String query = "select fname from students where studentno=17";
-
-			ResultSet rs = statement.executeQuery(query);
-
-			if (rs.next()) {
-				return rs.getString(1);
-			}
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return str;
 	}
 
 	public String getQRCode() throws Exception {
