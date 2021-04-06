3567f4a19d08d5199e6830b9081dfafd6dba9cfb
Harsh K <hkmkeswani@myseneca.ca>
2021-04-06 16:23:09 -0400

    semi functional login page
    need to figure out wallet/:id restriction for each account

--- btp400-a2/src/main/java/application/SecurityConfiguration.java
+++ btp400-a2/src/main/java/application/SecurityConfiguration.java
@@ -1,42 +1,40 @@
 package application;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-//	@Autowired
-//	DataSource dataSource;
+	@Autowired
+	DataSource dataSource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.jdbcAuthentication().dataSource(dataSource)
-//				.usersByUsernameQuery("select username,password, enabled from walletusers where username=?")
-//				.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
+		auth.jdbcAuthentication().dataSource(dataSource)
+				.usersByUsernameQuery("select username, password, enabled from walletusers where username=?")
+				.authoritiesByUsernameQuery("select username, role from user_roles where username=?");
+	}
 
-		auth.inMemoryAuthentication()
-			.withUser("harsh")
-			.password("123")
-			.roles("USER");
-	}
-	
+	// this doesnt encode, should replace with something proper
+	@SuppressWarnings("deprecation")
 	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+	public static NoOpPasswordEncoder passwordEncoder() {
+		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").hasRole("USER").and().formLogin().and().csrf().disable();
-//				.permitAll().and().logout().permitAll();
-//		http.exceptionHandling().accessDeniedPage("/403");
+		http.authorizeRequests().antMatchers("/**").hasAnyAuthority("USER").anyRequest().authenticated().and()
+				.formLogin().permitAll().and().logout().permitAll();
+
 	}
 
-	
 }
