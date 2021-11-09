c854ac2db6245b77dda1a6ea016720cdf64893c0
Justin K <mletemps@hotmail.com>
2021-11-09 00:27:37 -0500

    updated ip of db

--- btp400-a2/src/main/java/application/controllers/MainController.java
+++ btp400-a2/src/main/java/application/controllers/MainController.java
@@ -49,8 +49,8 @@ public class MainController {
 		if (accountNo == -1) {
 
 			try (Connection connection = DriverManager.getConnection(
-					"jdbc:postgresql://ec2-54-209-43-223.compute-1.amazonaws.com:5432/d19rc88931g1bi", "luwrnpzmqrzvln",
-					"9b76f4cfa5a87feb4cf28e8b90e485b183bca39b2b20c09e323b4a04b524b2ce");
+					"jdbc:postgresql://174.92.60.107:5432", "postgres",
+					"123456");
 					Statement statement = connection.createStatement()) {
 
 				String query = "select accno from walletusers where username='" + username + "';";
